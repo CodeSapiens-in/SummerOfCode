5435b8ab59b871266e122aeb6e4c50bf59cbca5a
JustinBenito <justinbenito1974@gmail.com>
2024-03-10 18:08:57 +0530

    new hot fix

--- app/typewriter.tsx
+++ app/typewriter.tsx
@@ -1,93 +1,6 @@
-import { useState, useEffect } from "react";
+import React from "react";
+import { motion } from "framer-motion";
 import { cn } from "./utils/cn";
-import { motion, stagger, useAnimate, useInView } from "framer-motion";
-
-export const TypewriterEffect = ({
-  words,
-  className,
-  cursorClassName,
-}: {
-  words: {
-    text: string;
-    className?: string;
-  }[];
-  className?: string;
-  cursorClassName?: string;
-}) => {
-  const [animationCompleted, setAnimationCompleted] = useState(false); // State to track if animation has already occurred
-  const [scope, animate] = useAnimate();
-  const isInView = useInView(scope);
-
-  useEffect(() => {
-    if (isInView && !animationCompleted) { // Check if component is in view and animation hasn't already completed
-      animate(
-        "span",
-        {
-          display: "inline-block",
-          opacity: 1,
-        },
-        {
-          duration: 0.3,
-          delay: stagger(0.1),
-          ease: "easeInOut",
-        }
-      );
-      setAnimationCompleted(true); // Set animationCompleted to true after animation
-    }
-  }, [isInView, animate, animationCompleted]);
-
-  const renderWords = () => {
-    return (
-      <motion.div ref={scope} className="inline">
-        {words.map((word, idx) => (
-          <div key={`word-${idx}`} className="inline-block">
-            {word.text.map((char, index) => (
-              <motion.span
-                initial={{}}
-                key={`char-${index}`}
-                className={cn(
-                  `dark:text-white text-black opacity-0 hidden`,
-                  word.className
-                )}
-              >
-                {char}
-              </motion.span>
-            ))}
-            &nbsp;
-          </div>
-        ))}
-      </motion.div>
-    );
-  };
-
-  return (
-    <div
-      className={cn(
-        "text-base sm:text-xl md:text-3xl lg:text-5xl font-bold text-center",
-        className
-      )}
-    >
-      {renderWords()}
-      <motion.span
-        initial={{
-          opacity: 0,
-        }}
-        animate={{
-          opacity: 1,
-        }}
-        transition={{
-          duration: 0.8,
-          repeat: Infinity,
-          repeatType: "reverse",
-        }}
-        className={cn(
-          "inline-block rounded-sm w-[4px] h-4 md:h-6 lg:h-10 bg-blue-500",
-          cursorClassName
-        )}
-      ></motion.span>
-    </div>
-  );
-};
 
 export const TypewriterEffectSmooth = ({
   words,
@@ -106,7 +19,7 @@ export const TypewriterEffectSmooth = ({
       <div>
         {words.map((word, idx) => (
           <div key={`word-${idx}`} className="inline-block">
-            {word.text.split("").map((char, index) => (
+            {word.text.split('').map((char, index) => ( // Spread operator used to convert string to array of characters
               <span
                 key={`char-${index}`}
                 className={cn(` text-white `, word.className)}
